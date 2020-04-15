4da606f2dc5672dfc5294614442639d846321c36
Denzel <chiagozieani145@gmail.com>
2020-04-15 10:17:52 +0100

    Now we can post up next we retrieve the data from the database

--- lib/Views/UI/dash_board.dart
+++ lib/Views/UI/dash_board.dart
@@ -1,4 +1,7 @@
 import 'package:big_field_data/Views/shared/dash_view.dart';
+import 'package:big_field_data/constants/routes.dart';
+import 'package:big_field_data/locator.dart';
+import 'package:big_field_data/services/navigation_services.dart';
 import 'package:big_field_data/view_models/dashboard_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider_architecture/provider_architecture.dart';
@@ -6,6 +9,8 @@ import 'package:provider_architecture/provider_architecture.dart';
 
 
 class Dashboard extends StatelessWidget {
+
+  final NavigationService navto = locator<NavigationService>();
   @override
   Widget build(BuildContext context) {
     return ViewModelProvider<DashboardViewModel>.withConsumer(
@@ -13,6 +18,12 @@ class Dashboard extends StatelessWidget {
       builder: (context,model,child) => DefaultTabController(
           length: 3,
           child: Scaffold(
+            floatingActionButton: FloatingActionButton(
+              onPressed: (){
+                navto.navigateTo(CreateFundsRoute);
+              },
+              child: Icon(Icons.add)
+            ),
             appBar: AppBar(bottom: TabBar(tabs: <Widget>[
                     Tab(child: Text('Recomendations'),),
                     Tab(child: Text('My Contributions'),),
